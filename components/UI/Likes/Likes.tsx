ce55aed88b7e1b5b975f0ee77b2bf28d03161869
netstat-ano <bartosz147258@gmail.com>
2023-06-22 18:08:38 +0200

    animation on like

--- components/UI/Likes/Likes.tsx
+++ components/UI/Likes/Likes.tsx
@@ -1,15 +1,21 @@
 import styles from "./Likes.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 const Likes: React.FC<{
     onLikeHandler: () => void;
     likes?: number;
     likeStatus: boolean;
 }> = (props) => {
+    const [beat, setBeat] = useState(false);
+    const onLike = () => {
+        props.onLikeHandler();
+        setBeat(true);
+    };
     return (
         <div
             data-testid="like"
-            onClick={props.onLikeHandler}
+            onClick={onLike}
             className={`
     ${styles["post-card__likes"]}
         ${
@@ -19,8 +25,15 @@ const Likes: React.FC<{
         }
     `}
         >
-            <FontAwesomeIcon icon={faPlus} />
-            {props.likes ? props.likes : 0}
+            <div
+                className={` ${beat ? styles["beat"] : ""}`}
+                onAnimationEnd={() => {
+                    setBeat(false);
+                }}
+            >
+                <FontAwesomeIcon icon={faPlus} />
+                {props.likes ? props.likes : 0}
+            </div>
         </div>
     );
 };
